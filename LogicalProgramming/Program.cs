a10161f4eefa6342bc843c83627301c632be598b
Jebakani <vjishwarya@gmail.com>
2021-06-30 19:07:44 +0530

    [JEBAKANI] Executing Logical Programming

---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programming");
+            Console.Read();
+        }
+    }
+}
